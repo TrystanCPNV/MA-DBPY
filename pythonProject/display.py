d9173734904c3224c0368b21aebc0e3a6fe52409
TrystanCPNV <trystan.gaillard@eduvaud.ch>
2025-09-10 11:05:09 +0200

    ajout des fichiers

---
+++ pythonProject/display.py
@@ -0,0 +1,17 @@
+import database
+import random
+import datetime
+
+
+def afficher_menu():
+    print("\n=== MENU ORDRE EN CLASSE ===")
+    print("1. Afficher l'ordre en classe")
+    print("2. Générer le planning « Ordre en classe »")
+    print("3. Valider l'ordre en classe de la semaine")
+    print("4. Supprimer un élève de la liste")
+    print("5. Ajouter un élève de la liste")
+    print("6. Générer le document « Ordre en classe »")
+    print("7. Sortir du menu")
+
+if __name__ == "__main__":
+    afficher_menu()
